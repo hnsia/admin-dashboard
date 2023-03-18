4f27b72bdf29ef7b90894ac692b53b1ae359a404
hnsia <44498178+hnsia@users.noreply.github.com>
2023-03-18 17:32:20 +0800

    Complete base calendar

--- admin-dashboard/src/scenes/calendar/index.jsx
+++ admin-dashboard/src/scenes/calendar/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import FullCalendar, { formatDate } from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
+import { formatDate } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
@@ -39,7 +40,7 @@ const Calendar = () => {
   const handleEventClick = (selected) => {
     if (
       window.confirm(
-        `Are you sure you wnat t odelete the event '${selected.event.title}'`
+        `Are you sure you wnat to delete the event '${selected.event.title}'`
       )
     ) {
       selected.event.remove();
@@ -50,7 +51,72 @@ const Calendar = () => {
     <Box m="20px">
       <Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
 
-      <Box display="flex" justifyContent="space-between"></Box>
+      <Box display="flex" justifyContent="space-between">
+        {/* Calendar Sidebar */}
+        <Box
+          flex="1 1 20%"
+          backgroundColor={colors.primary[400]}
+          p="15px"
+          borderRadius="4px"
+        >
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvents.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  backgroundColor: colors.greenAccent[500],
+                  margin: "10px 0",
+                  borderRadius: "2px",
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+
+        {/* Calendar */}
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              { id: "1234", title: "All-day event", date: "2023-03-14" },
+              { id: "4321", title: "Timed event", date: "2023-03-28" },
+            ]}
+          />
+        </Box>
+      </Box>
     </Box>
   );
 };
